102fec55df772ec452d934e5f232cb901a79b664
santino-gomez <white42331@gmail.com>
2025-11-05 21:23:44 -0300

    Delete proyecto v8 directory

--- proyecto v8/backend/php/imagotipo.php
+++
@@ -1,4 +0,0 @@
-<span class="imagotipo">
-    <img class="isotipo" src="images/ConectaUTU.svg" draggable="false" alt="Isotipo de ConectaUTU">
-    <h1 class="logotipo">ConectaUTU</h1>
-</span>
--- proyecto v8/frontend/js/mostrarClave.js
+++
@@ -1,9 +0,0 @@
-const passwordField = document.getElementById('passwordField');
-const togglePassword = document.getElementById('togglePassword');
-
-togglePassword.addEventListener('click', function () {
-    const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
-    passwordField.setAttribute('type', type);
-
-    this.textContent = type === 'password' ? 'Mostrar' : 'Ocultar';
-});
